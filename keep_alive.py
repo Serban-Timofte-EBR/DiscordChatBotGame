4d1f91055850dd6ff7f7b13c0edb566bc2188d7f
Serban-Timofte-EBR <108286629+Serban-Timofte-EBR@users.noreply.github.com>
2023-01-09 13:20:56 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,3 @@
-''The discord server which will host our chatbot! This server will stop running after one hour! I saw this code on GitHub here: https://gist.github.com/beaucarnes/51ec37412ab181a2e3fd320ee474b671 . I update it using uptimerobot.com where I created a monitor directly on the website which will pink the server every 5 minutes to keep it running!'''
 
 from flask import Flask        # use it like an webserver 
 from threading import Thread   # the server will run on other server of the bot so they will be able so run in the same time
